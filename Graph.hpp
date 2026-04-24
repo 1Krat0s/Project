4f9b379a1e030f62e61536fb14e6a8742ba24ab3
Tiffany Campbell <tcampbell7573@floridapoly.edu>
2026-04-24 00:45:58 +0000

    tasks 3 and 4 done

--- Graph.hpp
+++ Graph.hpp
@@ -16,8 +16,12 @@ class Graph
     private:
         std::vector<Airport> airports;
         int findAirportIndex(const std::string& code) const;
+        Airplane getPlane(int airport1, int airport2);
+        std::vector<std::vector<int>> AllPathsStops(int start, const int dest, int stops) const;
 
     public:
+        
+
         // Task 1: Construct weighted directed graph from CSV 
         bool loadFromCSV(const std::string& filename);
 
@@ -28,6 +32,12 @@ class Graph
         void displayConnectionCounts() const;
 
         int getNumAirports() const;
+
+        //task 3 - all airports in state (TC)
+        void allState(const std::string& origin, const std::string& stateAbb);
+
+        //task 4 - shortest path with stops
+        void shortestPathStops(const std::string& origin, const std::string& dest, int stops);
 };
 
 #endif
--- mainAirport.cpp
+++ mainAirport.cpp
@@ -8,14 +8,16 @@
 int main(void){
     Graph simGraph;
 
-    if (simGraph.loadFromCSV("airports.csv")){
+    if (simGraph.loadFromCSV("airports.csv")){ //task 1
         std::cout << "SUCCESS" << std::endl;
     }
     else{
         std::cout << "NOT FOUND" << std::endl;
     }
-    simGraph.displayConnectionCounts();
-    simGraph.shortestPath("IAD","MIA");
+    simGraph.displayConnectionCounts(); //task 5
+    simGraph.shortestPath("IAD","MIA"); //task 2
+    simGraph.allState("ATL", "FL"); //task 3
+    simGraph.shortestPathStops("IAD", "MIA", 3); //task 4
 
 
 }
